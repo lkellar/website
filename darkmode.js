9d42529e881bdb60a18a954668878e2efefde996
Lucas Kellar <katzrkool@protonmail.com>
2019-02-22 12:38:17 -0600

    updated darkmode to use local storage

--- darkmode.js
+++ darkmode.js
@@ -1,52 +1,17 @@
-function setCookie(name, value) {
-    document.cookie = name + '=' + value  + `; path=/; domain=${topLevelDomain()}; expires=${tenYears()}`;
-}
-
-function tenYears() {
-    const CookieDate = new Date;
-    CookieDate.setFullYear(CookieDate.getFullYear() +10);
-    return CookieDate.toUTCString();
-}
-
-function topLevelDomain() {
-    if (document.domain === '0.0.0.0') {
-        return document.domain;
-    }
-    return document.domain.split('.').slice(-2).join('.');
-}
-
-
-function getCookie(c_name) {
-    let c_start;
-    let c_end;
-    if (document.cookie.length > 0) {
-        c_start = document.cookie.indexOf(c_name + '=');
-        if (c_start !== -1) {
-            c_start = c_start + c_name.length + 1;
-            c_end = document.cookie.indexOf(';', c_start);
-            if (c_end === -1) {
-                c_end = document.cookie.length;
-            }
-            return unescape(document.cookie.substring(c_start, c_end));
-        }
-    }
-    return null;
-}
-
 function triggerDarkMode(on) {
     if (on) {
         changeSpanText(true);
-        setCookie('darkmode', 'true');
+        localStorage.setItem('darkmode', 'true');
     } else {
         changeSpanText(false);
-        setCookie('darkmode', 'false');
+        localStorage.setItem('darkmode', 'false');
     }
     document.body.classList.toggle('darkmode');
 }
 
 function toggle() {
-    const cookie = getCookie('darkmode');
-    if (cookie === 'true') {
+    const darkValue = localStorage.getItem('darkmode');
+    if (darkValue === 'true') {
       triggerDarkMode(false);
     } else {
         triggerDarkMode(true);
@@ -55,7 +20,6 @@ function toggle() {
 
 function changeSpanText(on) {
     const ele = document.getElementById('darkmode');
-    console.log(ele);
     if (ele) {
         if (on) {
             ele.getElementsByTagName('h1')[0].textContent = 'Deactivate Dark Mode';
@@ -78,10 +42,10 @@ function darkTest(e) {
 }
 
 function init() {
-    const cookie = getCookie('darkmode');
+    const darkValue = localStorage.getItem('darkmode');
     const mql = window.matchMedia('(prefers-color-scheme: dark)');
     mql.addListener(darkTest);
-    if (cookie === 'true' || mql.matches) {
+    if (darkValue === 'true' || mql.matches) {
         triggerDarkMode(true);
     }
 }
