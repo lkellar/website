828cb83d3effa1a027a4ba88ebaef493457b7cff
Lucas K <lucas@lkellar.org>
2019-03-28 21:22:39 -0500

    updated darkmode to respect user prefs

--- darkmode.js
+++ darkmode.js
@@ -1,18 +1,19 @@
-function triggerDarkMode(on) {
-    if (on) {
-        changeSpanText(true);
-        localStorage.setItem('darkmode', 'true');
-    } else {
-        changeSpanText(false);
-        localStorage.setItem('darkmode', 'false');
+function triggerDarkMode(on, set=true) {
+    if (set) {
+        localStorage.setItem('darkmode',  on.toString())
     }
-    document.body.classList.toggle('darkmode');
+    if (document.body.classList.contains('darkmode')) {
+        document.body.classList.remove('darkmode');
+        changeSpanText(false);
+    } else {
+        document.body.classList.add('darkmode');
+        changeSpanText(true);
+    }
 }
 
 function toggle() {
-    const darkValue = localStorage.getItem('darkmode');
-    if (darkValue === 'true') {
-      triggerDarkMode(false);
+    if (document.body.classList.contains('darkmode')) {
+        triggerDarkMode(false);
     } else {
         triggerDarkMode(true);
     }
@@ -34,19 +35,35 @@ function changeSpanText(on) {
 }
 
 function darkTest(e) {
-    if (e.matches) {
-        triggerDarkMode(true);
-    } else {
-        triggerDarkMode(false);
+    // If the user already has a pref set, we don't want to adjust the color based on system pref
+    if (!localStorage.getItem('darkmode')) {
+        if (e.matches) {
+            triggerDarkMode(true, false);
+        } else {
+            triggerDarkMode(false, false);
+        }
     }
 }
 
 function init() {
-    const darkValue = localStorage.getItem('darkmode');
+    const darkPref = localStorage.getItem('darkmode');
     const mql = window.matchMedia('(prefers-color-scheme: dark)');
     mql.addListener(darkTest);
-    if (darkValue === 'true' || mql.matches) {
-        triggerDarkMode(true);
+
+    // Some of the things in the statements below are redundant, but I really want to make sure someone's preference
+    // doesn't get messed up.
+    if (darkPref) {
+        if (darkPref === 'true') {
+            triggerDarkMode(true);
+        } else if (darkPref === 'false') {
+            triggerDarkMode(false);
+        }
+    } else {
+        if (mql.matches) {
+            triggerDarkMode(true, false);
+        } else {
+            triggerDarkMode(false, false);
+        }
     }
 }
 
