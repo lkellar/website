b52d0649ee38e81670a9d9a280ebf5144244cd5a
Lucas K <lucas@lkellar.org>
2026-05-03 20:09:00 -0400

    less ehcoing

--- endpoints/check_whats_new.sh
+++ endpoints/check_whats_new.sh
@@ -4,12 +4,8 @@ if [ -d "../whats_new/" ]; then
     if [ -f "../whats_new/expiry.txt" ]; then
         expiry_time=$(<"../whats_new/expiry.txt")
         now=$(date +%s);
-        echo $expiry_time;
-        echo $now;
         if [ "$expiry_time" -lt "$now" ]; then
             rm -r '../whats_new/'
-            echo 'BYE';
         fi
-        echo 'HI';
     fi
 fi
