8f22431533b9579cf8667ee258d3de682fca08c5
Lucas K <lucas@lkellar.org>
2026-05-13 17:27:00 -0500

    update script and change extension

--- endpoints/check_whats_new.sh
+++ endpoints/check_whats_new.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# needs to be run in this directory
 
 if [ -d "../whats_new/" ]; then
     if [ -f "../whats_new/expiry.txt" ]; then
-        expiry_time=$(<"../whats_new/expiry.txt")
+        expiry_time=$(<"../whats_new/expiry.txt");
         now=$(date +%s);
         if [ "$expiry_time" -lt "$now" ]; then
-            rm -r '../whats_new/'
+            rm "../whats_new/*";
         fi
+        touch "../whats_new/index.html";
     fi
 fi
